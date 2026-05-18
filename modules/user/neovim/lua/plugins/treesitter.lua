6f7f05f5007a4cddad223e8478f371abb8e675a7
hideshots <88557210+hideshots@users.noreply.github.com>
2026-05-19 01:09:59 +0300

    nvim

--- modules/user/neovim/lua/plugins/treesitter.lua
+++ modules/user/neovim/lua/plugins/treesitter.lua
@@ -1,27 +1,43 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = { "neovim-treesitter/treesitter-parser-registry" },
+  lazy = false,
   build = ":TSUpdate",
   config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ignore_install = {},
-      ensure_installed = {
-        "c",
-        "cpp",
-        "cmake",
-        "lua",
-        "bash",
-        "json",
-        "yaml",
-        "markdown",
-      },
-      highlight = {
-        enable = true,
-      },
-      indent = { enable = true },
-      modules = {},
-      sync_install = true,
-      auto_install = true,
+    local parsers = {
+      "c",
+      "cpp",
+      "cmake",
+      "lua",
+      "bash",
+      "json",
+      "yaml",
+      "markdown",
+      "markdown_inline",
+    }
+
+    local treesitter = require("nvim-treesitter")
+    treesitter.setup({
+      install_dir = vim.fn.stdpath("data") .. "/site",
+    })
+
+    local missing = vim.tbl_filter(function(parser)
+      return #vim.api.nvim_get_runtime_file("parser/" .. parser .. ".so", false) == 0
+    end, parsers)
+    if #missing > 0 and vim.fn.executable("tree-sitter") == 1 then
+      vim.schedule(function()
+        treesitter.install(missing)
+      end)
+    end
+
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("user_treesitter", { clear = true }),
+      pattern = parsers,
+      callback = function()
+        if pcall(vim.treesitter.start) then
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      end,
     })
   end
 }
